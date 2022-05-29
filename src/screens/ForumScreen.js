7aa75b557fdac4213ceb96140068ca68ab67bdb8
Felicia Gan <felicia.gan@me.com>
2022-05-29 23:18:56 +0800

    added main screens rough designs

--- src/screens/ForumScreen.js
+++ src/screens/ForumScreen.js
@@ -1,10 +1,66 @@
-import {Text, SafeAreaView} from 'react-native';
-import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import colors from '../assets/colors/colors.js';
+import Feather from 'react-native-vector-icons/Feather';
 
 export default function ForumScreen() {
+    const [activeTab, setActiveTab] = useState('Newest');
+
     return (
-        <SafeAreaView>
-            <Text>Forum</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <SafeAreaView style={styles.headerContainer}>
+                <Text style={styles.introtext}>Forum</Text>
+                <TouchableOpacity style={styles.searchIcon}>
+                    <Feather name="search" size={40} color={colors.lightGray}></Feather>
+                </TouchableOpacity>
+            </SafeAreaView>
+            <SafeAreaView style={styles.buttonContainer}>
+                <TabButton text={'Newest'} activeTab={activeTab} setActiveTab={setActiveTab}/>
+                <TabButton text={'Most liked'} activeTab={activeTab} setActiveTab={setActiveTab}/>
+            </SafeAreaView>
+        </View>
     );
 }
+
+const TabButton = (props) => {
+    return (
+    <TouchableOpacity 
+        style={{
+            backgroundColor: colors.lightBlue,
+            paddingVertical: 25,
+            paddingHorizontal: 55,
+        }}
+        onPress={() => props.setActiveTab(props.text)}>
+            <Text style={{
+                color: props.activeTab === props.text ? colors.darkBlue : colors.blue,
+                fontSize: 20,
+                fontWeight: 'bold',
+            }}>{props.text}</Text>
+    </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.pink,
+        flex: 1,
+    },
+    headerContainer: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+    },
+    introtext: {
+        color: colors.darkBlue,
+        fontWeight: 'bold',
+        fontSize: 40,
+        paddingVertical: 20,
+        paddingLeft: 10,
+    },
+    searchIcon: {
+        paddingVertical: 20,
+        paddingRight: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+    }
+});
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,52 @@
-import {Text, SafeAreaView} from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
+import colors from '../assets/colors/colors.js';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function ProfileScreen() {
     return (
-        <SafeAreaView>
-            <Text>Profile</Text>
-        </SafeAreaView>
+        <View>
+            <SafeAreaView style={styles.headerContainer}>
+                <Text style={styles.header}>User</Text>
+                <TouchableOpacity style={styles.settingsIcon}>
+                    <MaterialIcons name="settings" size={40} color={colors.lightGray}></MaterialIcons>
+                </TouchableOpacity>
+            </SafeAreaView>
+
+            <View style={{height: 245}}></View>
+
+            <View style={styles.profileContainer}>
+                <Text style={styles.userInfo}>User Information</Text>
+            </View>
+        </View>
     );
 }
+
+const styles = StyleSheet.create ({
+    headerContainer: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+    },
+    header: {
+        color: colors.darkBlue,
+        fontSize: 40,
+        fontWeight: 'bold',
+        paddingLeft: 10,
+        paddingVertical: 20,
+    },
+    settingsIcon: {
+        paddingVertical: 20,
+        paddingRight: 10,
+    },
+    profileContainer: {
+        backgroundColor: colors.pink,
+        borderRadius: 15,
+        height: 350,
+        
+    },
+    userInfo: {
+        color: colors.darkBlue,
+        fontSize: 30,
+        fontWeight: 'bold',
+    }
+});
